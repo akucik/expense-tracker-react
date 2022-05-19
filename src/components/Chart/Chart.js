9125e557589e85a9bce2aad77fcee3742367300a
Aneta Kucik <71226541+akucik@users.noreply.github.com>
2022-05-19 15:37:42 +0100

    add expense chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,8 +4,10 @@ import ChartBar from "./ChartBar";
 //dynamically render listOfItems(dataPoints) as <ul>, then pass some data to chartBar to control what value is rendered(displayed).
 //for Key in this example, we use label
 const Chart = (props) => {
+  console.log(props.items);
   const expenseValue = props.items.map((item) => item.value);
   const maxChartValue = Math.max(...expenseValue);
+
   return (
     <div className="chart">
       {props.items.map((item) => (
--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -8,6 +8,12 @@ const ExpensesChart = (props) => {
     { label: "Apr", value: 0 },
     { label: "May", value: 0 },
     { label: "Jun", value: 0 },
+    { label: "Jul", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sep", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
   ];
 
   //create 'for' loop:
@@ -18,6 +24,6 @@ const ExpensesChart = (props) => {
   //above: we increase the value of the month by expense amount.
   //after the looping the values in chartDataPoints, are no longer'0' but will be updated with expense amount.
 
-  return <Chart item={chartDataPoints} />;
+  return <Chart items={chartDataPoints} />;
 };
 export default ExpensesChart;
